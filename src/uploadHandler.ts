64ab06d20d83b28576a05254e325f8de3f01a38b
mukkumayc <sk9078@ya.ru>
2022-03-01 16:29:44 +0300

    Refactor

--- src/uploadHandler.ts
+++ src/uploadHandler.ts
@@ -1,27 +1,34 @@
 import type { Client } from "minio";
-import { fileDownloader, fileUploader } from "./minioHelpers";
 import type NotificationRecord from "./NotificationRecord";
 import { cutParentDir } from "./utils";
 import videoScaler from "./videoScaler";
+import fs from "fs/promises";
 
 const uploadHandler =
 	(client: Client) => async (record: NotificationRecord) => {
 		const bucketName = record.s3.bucket.name;
 		const objectPath = record.s3.object.key;
 
-		const videoPath = await fileDownloader(client)(
-			bucketName,
-			objectPath,
-			`uploads/${objectPath}`
-		);
+		const videoPath = `uploads/${objectPath}`;
+
+		await client.fGetObject(bucketName, objectPath, videoPath);
+
 		console.log(new Date(), "Get upload! File:", `${bucketName}/${objectPath}`);
 
 		const resolutions = [240, 360, 480, 720, 1080] as const;
-		Promise.all(resolutions.map(videoScaler(videoPath))).then((path) =>
-			path.map((path) =>
-				fileUploader(client)("processed-videos")(path, cutParentDir(path))
+		const processedVideoPaths = await Promise.all(
+			resolutions.map(videoScaler(videoPath))
+		);
+
+		await Promise.all(
+			processedVideoPaths.map((path) =>
+				client.fPutObject("processed-videos", cutParentDir(path), path, {
+					"Content-Type": "video/mp4",
+				})
 			)
 		);
+
+		return fs.rm("uploads", { recursive: true, force: true });
 	};
 
 export default uploadHandler;
